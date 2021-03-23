54deb22bc6952d87da702d659c8eb9c9b3654449
Par <pacroy@gmail.com>
2021-03-23 23:55:01 +0700

    Add checking variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,22 @@
 set -o errexit
 set -o pipefail
 
+printf "\nChecking variables...\n"
+[ -z "${CLIENT_ID}" ] && >&2 echo "CLIENT_ID is not set" && is_error="true"
+[ -z "${CLIENT_SECRET}" ] && >&2 echo "CLIENT_SECRET is not set" && is_error="true"
+[ -z "${TENANT_ID}" ] && >&2 echo "TENANT_ID is not set" && is_error="true"
+[ -z "${RESOURCE_GROUP}" ] && >&2 echo "RESOURCE_GROUP is not set" && is_error="true"
+[ -z "${DNSZONE}" ] && >&2 echo "DNSZONE is not set" && is_error="true"
+[ -z "${RECORD_NAME}" ] && >&2 echo "RECORD_NAME is not set" && is_error="true"
+[ -z "${COMMAND_IP}" ] && >&2 echo "COMMAND_IP is not set. Use default value 'curl -fsSL ipv4.icanhazip.com'"
+[ -n "${is_error}" ] && exit 90
+
 printf "\nLogging in...\n"
 az login --service-principal -u "${CLIENT_ID}" -p "${CLIENT_SECRET}" --tenant "${TENANT_ID}" --output table
 
 printf "\nChecking IP...\n"
-dns_ip=$(az network dns record-set a show --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "{RECORD_NAME}")
-cmd_ip=$(${COMMAND_IP})
+dns_ip=$(az network dns record-set a show --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}")
+cmd_ip=$(${COMMAND_IP:-"curl -fsSL ipv4.icanhazip.com"})
 printf "  DNS IP: %s\n" "${dns_ip}"
 printf "  CMD IP: %s\n" "${cmd_ip}"
 
