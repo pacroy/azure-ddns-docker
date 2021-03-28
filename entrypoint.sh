2d2c02b51a3896faf71c538a7e4117be30437d56
Par <pacroy@gmail.com>
2021-03-28 21:50:53 +0700

    Fix syntax in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,10 +21,10 @@ cmd_ip=$(${COMMAND_IP:-"curl -fsSL ipv4.icanhazip.com"})
 printf "  DNS IP: %s\n" "${dns_ip}"
 printf "  CMD IP: %s\n" "${cmd_ip}"
 
-if [ "${dns_ip}" != "${cmd_ip" ]; then
+if [ "${dns_ip}" != "${cmd_ip}" ]; then
     printf "\nUpdating IP...\n"
-    az network dns record-set a update --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "{RECORD_NAME}" --remove arecords 0
-    az network dns record-set a update --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "{RECORD_NAME}" --add arecords ipv4Address="${cmd_ip}"
+    az network dns record-set a update --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}" --remove arecords 0
+    az network dns record-set a update --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}" --add arecords ipv4Address="${cmd_ip}"
 else
     printf "\nNo update required\n"
 fi
