0601d4e76d5c6eb74bb1b9e7616a3af6f2172f4f
Par <pacroy@gmail.com>
2021-03-30 01:18:45 +0700

    Initial version (#1)
    * Try to build from alpine
    
    * Try MS/azure-cli image
    
    * Install dig from bind-tools
    
    * Add service principal creation
    
    * Add update script
    
    * Setup environment
    
    * Add ENTRYPOINT, but commented out
    
    * Add checking variables
    
    * Add Execute section
    
    * Fix getting dns_ip
    
    * Add Execute -> Docker section
    
    * Uncomment ENTRYPOINT
    
    * Fix syntax in entrypoint.sh
    
    * Redirect ouput
    
    * Add superlinter
    
    * Fix linting issue in Dockerfile
    
    * Update README with more explanation
    
    * Show current record-set when no update

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # azure-ddns-docker
-Docker image for Dynamic DNS with Azure DNS
+
+Docker image to update Azure DNS records to mimic Dynamic DNS service.
+
+## Service Principal
+
+Create a new service principal for querying and updating Azure DNS records
+
+```sh
+az ad sp create-for-rbac --name "{app-id-uri}" --role Contributor --scope "{resource-id}"
+```
+
+| Parameter | Description |
+| --- | --- |
+| app-id-uri | Application ID URI<br />Example: `sp://ddns-updater` |
+| resource-id | DNS Zone Resource ID<br />Example: `/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/rg-mygroup/providers/Microsoft.Network/dnszones/mydomain.com` |
+
+## Execute
+
+Execute these commands to build and run the docker image
+
+```sh
+docker build --no-cache -t azure-ddns .
+docker run -it --rm \          
+    -e CLIENT_ID="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" \
+    -e CLIENT_SECRET="your-client-secret" \
+    -e TENANT_ID="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" \
+    -e RESOURCE_GROUP="rg-mygroup" \
+    -e DNSZONE="mydomain.com" \
+    -e RECORD_NAME="@" \
+    -e COMMAND_IP="dig +short myotherdomain.com" \
+    azure-ddns
+```
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+printf "\nChecking variables...\n"
+[ -z "${CLIENT_ID}" ] && >&2 echo "CLIENT_ID is not set" && is_error="true"
+[ -z "${CLIENT_SECRET}" ] && >&2 echo "CLIENT_SECRET is not set" && is_error="true"
+[ -z "${TENANT_ID}" ] && >&2 echo "TENANT_ID is not set" && is_error="true"
+[ -z "${RESOURCE_GROUP}" ] && >&2 echo "RESOURCE_GROUP is not set" && is_error="true"
+[ -z "${DNSZONE}" ] && >&2 echo "DNSZONE is not set" && is_error="true"
+[ -z "${RECORD_NAME}" ] && >&2 echo "RECORD_NAME is not set" && is_error="true"
+[ -z "${COMMAND_IP}" ] && >&2 echo "COMMAND_IP is not set. Use default value 'curl -fsSL ipv4.icanhazip.com'"
+[ -n "${is_error}" ] && exit 90
+
+printf "\nLogging in...\n"
+az login --service-principal -u "${CLIENT_ID}" -p "${CLIENT_SECRET}" --tenant "${TENANT_ID}" --output table
+
+printf "\nChecking IP...\n"
+dns_ip=$(az network dns record-set a show --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}" | jq -r '.arecords[0].ipv4Address')
+cmd_ip=$(${COMMAND_IP:-"curl -fsSL ipv4.icanhazip.com"})
+printf "  DNS IP: %s\n" "${dns_ip}"
+printf "  CMD IP: %s\n" "${cmd_ip}"
+
+if [ "${dns_ip}" != "${cmd_ip}" ]; then
+    printf "\nUpdating IP...\n"
+    az network dns record-set a update --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}" --remove arecords 0  > /dev/null
+    az network dns record-set a update --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}" --add arecords ipv4Address="${cmd_ip}"
+else
+    printf "\nNo update required\n"
+    az network dns record-set a show --resource-group "${RESOURCE_GROUP}" --zone-name "${DNSZONE}" --name "${RECORD_NAME}"
+fi
