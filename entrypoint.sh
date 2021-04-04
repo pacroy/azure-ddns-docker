acc5366ddd4fbb60d6ef925ca6cc1cac3b33a802
Par <pacroy@gmail.com>
2021-04-04 12:47:17 +0700

    Fix check UPDATE_IP

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ az login --service-principal -u "${CLIENT_ID}" -p "${CLIENT_SECRET}" --tenant "$
 
 printf "\nGetting update IP...\n"
 UPDATE_IP=$(${UPDATE_IP_CMD:-"curl -fsSL ipv4.icanhazip.com"})
-[ -z "UPDATE_IP" ] && >&2 echo "Failed to get the update IP" && exit 91
+[ -z "${UPDATE_IP}" ] && >&2 echo "Failed to get the update IP" && exit 91
 
 for record in "$RECORD_NAMES"
 do
